e571a9a69bda6ade8a72238a3adbd83f8e87abce
Axel <axel@cedercentral.com>
2019-10-17 21:02:32 +0200

    Started process of adding posts

--- projects/index.php
+++ projects/index.php
@@ -1,8 +1,20 @@
+<?php 
+		session_start();
+		// connect to database
+		$conn = mysqli_connect("localhost", "root", "", "site_postings");
+
+		if (!$conn) {
+			die("Error connecting to database: " . mysqli_connect_error());
+		}
+		// define global constants
+		define ('ROOT_PATH', realpath(dirname(__FILE__)));
+		define('BASE_URL', 'http://localhost/complete-blog-php/');
+?>
+
 <! doctype html>
 
-
-
 <html>
+
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,13 +22,41 @@
 		<link rel="stylesheet" href="../css/styles.css">
 		<script type="text/javascript" src="../js/jquery-3.1.0.min.js"></script>
 		<link rel="shortcut icon" href="../resources/image/logo/ico.png" type="image/png">
+		
 	</head>
 	
+	
+
 	<?php include '../header.php';?>
 	<body>
 		<div id="fullpage">
 		<div class="section" id="section2" style="height:100%">
 		<h1><?php echo $lang['project']?></h1>
+		</div>
+		<div class="container">
+
+		<!-- Page content -->
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+			<!-- more content still to come here ... -->
+		</div>
+			<!-- // navbar -->
+
+			<!-- Page content -->
+			<div class="content">
+				<h2 class="content-title">Recent Articles</h2>
+				<hr>
+				<!-- more content still to come here ... -->
+			</div>
+			<!-- // Page content -->
+
+			<!-- footer -->
+			<div class="footer">
+				<p>MyViewers &copy; <?php echo date('Y'); ?></p>
+			</div>
+			<!-- // footer -->
+
 		</div>
 		<div id="proj1">
 			Rebuilding site to make room for this: <br>
