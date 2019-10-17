f5b5c99dcbd99781936bb8ad851d3b2ed20f3d34
Axel <axel@cedercentral.com>
2019-10-17 21:08:46 +0200

    'Fixed'

--- projects/index.php
+++ projects/index.php
@@ -1,14 +1,14 @@
 <?php 
 		session_start();
 		// connect to database
-		$conn = mysqli_connect("localhost", "root", "", "site_postings");
+		$conn = mysqli_connect("localhost", "phpmyadmin:", "ex04390eGiW9", "site_postings");
 
 		if (!$conn) {
 			die("Error connecting to database: " . mysqli_connect_error());
 		}
 		// define global constants
 		define ('ROOT_PATH', realpath(dirname(__FILE__)));
-		define('BASE_URL', 'http://localhost/complete-blog-php/');
+		define('BASE_URL', 'http://localhost/projects/');
 ?>
 
 <! doctype html>
